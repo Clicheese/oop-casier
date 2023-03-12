3afbb19613b936b9ea4a196bed6c57587ba064df
Clicheese <3103120021@student.smktelkom-pwt.sch.id>
2023-03-12 20:45:56 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace FormCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boxkodebarang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxnamabarang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.boxhargabeli = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.boxhargajual = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.boxsatuan = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.boxjumlahbarang = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(748, 55);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(345, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "zeamart";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // boxkodebarang
+            // 
+            this.boxkodebarang.BackColor = System.Drawing.Color.Silver;
+            this.boxkodebarang.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxkodebarang.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxkodebarang.Location = new System.Drawing.Point(25, 123);
+            this.boxkodebarang.Name = "boxkodebarang";
+            this.boxkodebarang.Size = new System.Drawing.Size(233, 20);
+            this.boxkodebarang.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kode Barang";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nama Barang";
+            // 
+            // boxnamabarang
+            // 
+            this.boxnamabarang.BackColor = System.Drawing.Color.Silver;
+            this.boxnamabarang.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxnamabarang.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxnamabarang.Location = new System.Drawing.Point(25, 177);
+            this.boxnamabarang.Name = "boxnamabarang";
+            this.boxnamabarang.Size = new System.Drawing.Size(233, 20);
+            this.boxnamabarang.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Harga Beli";
+            // 
+            // boxhargabeli
+            // 
+            this.boxhargabeli.BackColor = System.Drawing.Color.Silver;
+            this.boxhargabeli.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxhargabeli.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxhargabeli.Location = new System.Drawing.Point(25, 284);
+            this.boxhargabeli.Name = "boxhargabeli";
+            this.boxhargabeli.Size = new System.Drawing.Size(233, 20);
+            this.boxhargabeli.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Harga Jual";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // boxhargajual
+            // 
+            this.boxhargajual.BackColor = System.Drawing.Color.Silver;
+            this.boxhargajual.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxhargajual.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxhargajual.Location = new System.Drawing.Point(25, 230);
+            this.boxhargajual.Name = "boxhargajual";
+            this.boxhargajual.Size = new System.Drawing.Size(233, 20);
+            this.boxhargajual.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Satuan";
+            // 
+            // boxsatuan
+            // 
+            this.boxsatuan.BackColor = System.Drawing.Color.Silver;
+            this.boxsatuan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxsatuan.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxsatuan.Location = new System.Drawing.Point(25, 395);
+            this.boxsatuan.Name = "boxsatuan";
+            this.boxsatuan.Size = new System.Drawing.Size(233, 20);
+            this.boxsatuan.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Jumlah Barang";
+            // 
+            // boxjumlahbarang
+            // 
+            this.boxjumlahbarang.BackColor = System.Drawing.Color.Silver;
+            this.boxjumlahbarang.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.boxjumlahbarang.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxjumlahbarang.Location = new System.Drawing.Point(25, 341);
+            this.boxjumlahbarang.Name = "boxjumlahbarang";
+            this.boxjumlahbarang.Size = new System.Drawing.Size(233, 20);
+            this.boxjumlahbarang.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(286, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(487, 206);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSlateGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(286, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(487, 29);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SlateGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(286, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(487, 29);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "load data";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SlateGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(286, 374);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(487, 29);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "update";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SlateGray;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(286, 409);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(487, 29);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "delete";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.boxsatuan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.boxjumlahbarang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.boxhargabeli);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.boxhargajual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.boxnamabarang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.boxkodebarang);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox boxkodebarang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox boxnamabarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox boxhargabeli;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox boxhargajual;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox boxsatuan;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox boxjumlahbarang;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
